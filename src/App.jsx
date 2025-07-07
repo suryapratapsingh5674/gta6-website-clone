3b301f94610f3db13049f9ec7f44ab242b15d531
surya pratap singh tomar <suryat6574@gmail.com>
2025-07-07 12:07:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+const App = () => {
+  return (
+    <div className='text-3xl text-yellow-400'>App</div>
+  )
+}
+
+export default App
